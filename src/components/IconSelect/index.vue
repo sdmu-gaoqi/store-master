19a70fba8e7413a680fa9c153fb69c739687b0cf
gaoqi <1224362143@qq.com>
2023-11-18 14:12:44 +0800

    chore: 修改icon

--- src/components/IconSelect/index.vue
+++ src/components/IconSelect/index.vue
@@ -1,15 +1,31 @@
 <!-- @author zhengjie -->
 <template>
   <div class="icon-body">
-    <el-input v-model="name" class="icon-search" clearable placeholder="请输入图标名称" @clear="filterIcons" @input="filterIcons">
+    <el-input
+      v-model="name"
+      class="icon-search"
+      clearable
+      placeholder="请输入图标名称"
+      @clear="filterIcons"
+      @input="filterIcons"
+    >
       <i slot="suffix" class="el-icon-search el-input__icon" />
     </el-input>
     <div class="icon-list">
       <div class="list-container">
-        <div v-for="(item, index) in iconList" class="icon-item-wrapper" :key="index" @click="selectedIcon(item)">
+        <div
+          v-for="(item, index) in iconList"
+          class="icon-item-wrapper"
+          :key="index"
+          @click="selectedIcon(item)"
+          style="background: #4d4def8c"
+        >
           <div :class="['icon-item', { active: activeIcon === item }]">
-            <svg-icon :icon-class="item" class-name="icon" style="height: 25px;width: 16px;"/>
-            <span>{{ item }}</span>
+            <img
+              :src="item"
+              class-name="icon"
+              style="height: 25px; width: 16px"
+            />
           </div>
         </div>
       </div>
@@ -18,87 +34,89 @@
 </template>
 
 <script>
-import icons from './requireIcons'
+import icons from "./requireIcons";
 export default {
-  name: 'IconSelect',
+  name: "IconSelect",
   props: {
     activeIcon: {
-      type: String
-    }
+      type: String,
+    },
   },
   data() {
     return {
-      name: '',
-      iconList: icons
-    }
+      name: "",
+      iconList: icons,
+    };
   },
   methods: {
     filterIcons() {
-      this.iconList = icons
+      this.iconList = icons;
       if (this.name) {
-        this.iconList = this.iconList.filter(item => item.includes(this.name))
+        this.iconList = this.iconList.filter((item) =>
+          item.includes(this.name)
+        );
       }
     },
     selectedIcon(name) {
-      this.$emit('selected', name)
-      document.body.click()
+      this.$emit("selected", name);
+      document.body.click();
     },
     reset() {
-      this.name = ''
-      this.iconList = icons
-    }
-  }
-}
+      this.name = "";
+      this.iconList = icons;
+    },
+  },
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  .icon-body {
-    width: 100%;
-    padding: 10px;
-    .icon-search {
-      position: relative;
-      margin-bottom: 5px;
-    }
-    .icon-list {
-      height: 200px;
-      overflow: auto;
-      .list-container {
+.icon-body {
+  width: 100%;
+  padding: 10px;
+  .icon-search {
+    position: relative;
+    margin-bottom: 5px;
+  }
+  .icon-list {
+    height: 200px;
+    overflow: auto;
+    .list-container {
+      display: flex;
+      flex-wrap: wrap;
+      .icon-item-wrapper {
+        width: calc(100% / 3);
+        height: 25px;
+        line-height: 25px;
+        cursor: pointer;
         display: flex;
-        flex-wrap: wrap;
-        .icon-item-wrapper {
-          width: calc(100% / 3);
-          height: 25px;
-          line-height: 25px;
-          cursor: pointer;
+        .icon-item {
           display: flex;
-          .icon-item {
-            display: flex;
-            max-width: 100%;
-            height: 100%;
-            padding: 0 5px;
-            &:hover {
-              background: #ececec;
-              border-radius: 5px;
-            }
-            .icon {
-              flex-shrink: 0;
-            }
-            span {
-              display: inline-block;
-              vertical-align: -0.15em;
-              fill: currentColor;
-              padding-left: 2px;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              white-space: nowrap;
-            }
-          }
-          .icon-item.active {
+          max-width: 100%;
+          height: 100%;
+          padding: 0 5px;
+          &:hover {
             background: #ececec;
             border-radius: 5px;
           }
+          .icon {
+            flex-shrink: 0;
+          }
+          span {
+            display: inline-block;
+            vertical-align: -0.15em;
+            fill: currentColor;
+            padding-left: 2px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+        }
+        .icon-item.active {
+          background: #ececec;
+          border-radius: 5px;
         }
       }
     }
   }
+}
 </style>
--- src/components/IconSelect/requireIcons.js
+++ src/components/IconSelect/requireIcons.js
@@ -1,11 +1,31 @@
+const req = require.context("../../assets/icons/svg", false, /\.svg$/);
+const requireAll = (requireContext) => requireContext.keys();
 
-const req = require.context('../../assets/icons/svg', false, /\.svg$/)
-const requireAll = requireContext => requireContext.keys()
+const re = /\.\/(.*)\.svg/;
 
-const re = /\.\/(.*)\.svg/
+// const icons = requireAll(req).map(i => {
+//   return i.match(re)[1]
+// })
+const icons = [
+  "http://111.229.138.125/icons/创建订单.svg",
+  "http://111.229.138.125/icons/工作台.svg",
+  "http://111.229.138.125/icons/订单.svg",
+  "http://111.229.138.125/icons/会员.svg",
+  "http://111.229.138.125/icons/会员卡.svg",
+  "http://111.229.138.125/icons/角色.svg",
+  "http://111.229.138.125/icons/客户管理.svg",
+  "http://111.229.138.125/icons/门店.svg",
+  "http://111.229.138.125/icons/日志.svg",
+  "http://111.229.138.125/icons/设置.svg",
+  "http://111.229.138.125/icons/收起.svg",
+  "http://111.229.138.125/icons/文档管理-项目类文档.svg",
+  "http://111.229.138.125/icons/员工.svg",
+  "http://111.229.138.125/icons/运营.svg",
+  "http://111.229.138.125/icons/运营.svg",
+  "http://111.229.138.125/icons/en.webp",
+  "http://111.229.138.125/loginBanner.png",
+  "http://111.229.138.125/vip.svg",
+  "http://111.229.138.125/vue.svg",
+];
 
-const icons = requireAll(req).map(i => {
-  return i.match(re)[1]
-})
-
-export default icons
+export default icons;
